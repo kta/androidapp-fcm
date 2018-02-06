00863d8514b8cfcb03343366f25aad6dfd821fe8
sakakibara-tetsu <sakakibara-tetsu@dmm.com>
2018-02-06 15:57:51 +0900

    fcm

--- app/src/main/java/com/example/sakakibara_tetsu/push/MainActivity.java
+++ app/src/main/java/com/example/sakakibara_tetsu/push/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.sakakibara_tetsu.push;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ここでタグの設定をする。 このタグに対してメッセージを送るので、iOS, Android, AdndroidPlusとそれぞれ設定すると良いかも
+        FirebaseMessaging.getInstance().subscribeToTopic("Android");
+        // ここでタグの設定をする。 アプリ全体にもメッセージを送りたい場合のために、共通するタグの設定も行う
+        FirebaseMessaging.getInstance().subscribeToTopic("app");
     }
 }
